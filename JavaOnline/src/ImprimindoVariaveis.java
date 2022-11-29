5772e2290a7dc7e900ae279abaf2c6bb1e549e89
Erivelton Laia <eriveltonll@gmail.com>
2022-11-29 19:42:31 -0300

    Tipo primitivo int

--- JavaOnline/src/ImprimindoVariaveis.java
+++ JavaOnline/src/ImprimindoVariaveis.java
@@ -1,16 +1,9 @@
 public class ImprimindoVariaveis {
-    //Pode escrever o que quiser
 
-    /*
-    Comentário maior com várias linhas
-    */
-
-    /***
-     * Esse é o método main
-     * @param args parametro usado para o metodo main
-     */
 
     public static void main(String[] args) {
-        System.out.println("Dados Impressos");
+
+        int idadeDosPaisParaCadastro = 18;
+        System.out.println("A idade é: " + idadeDosPaisParaCadastro);
     }
 }
