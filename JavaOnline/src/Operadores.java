88793cbdc1fccbbc7641071275ecaafaaa01fe82
Erivelton Laia <eriveltonll@gmail.com>
2022-11-29 19:57:33 -0300

    Operadores Primarios

---
+++ JavaOnline/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 5;
+
+
+        System.out.println("A soma é: " + (n1+n2));
+        System.out.println("A subtração é: " + (n1-n2));
+        System.out.println("A divisão é: " + (n1/n2));
+        System.out.println("A multiplicação é: " + (n1*n2));
+    }
+}
