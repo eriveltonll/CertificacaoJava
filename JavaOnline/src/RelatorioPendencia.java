f9411033b8d2f8ef554fcfa1a288d1111ca42142
Erivelton Laia <eriveltonll@gmail.com>
2022-11-29 19:52:50 -0300

    Tipos primitivos com exercicios

---
+++ JavaOnline/src/RelatorioPendencia.java
@@ -0,0 +1,11 @@
+public class RelatorioPendencia {
+
+    public static void main(String[] arqs) {
+
+        String nome = "Erivelton";
+        String endereco = "Rua A, 38, Vila";
+        String telefone = "(33) 3333-2221";
+
+        System.out.println("O " + nome + " domiciliado no endereco " + endereco + " e telefone " + telefone + " não possui pendencias;");
+    }
+}
