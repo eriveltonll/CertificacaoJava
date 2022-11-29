15ba35ee6d51c3eecaf2234d9351057f5db0139f
Erivelton Laia <eriveltonll@gmail.com>
2022-11-29 19:39:20 -0300

    Tipos de comentários

---
+++ JavaOnline/src/ImprimindoVariaveis.java
@@ -0,0 +1,16 @@
+public class ImprimindoVariaveis {
+    //Pode escrever o que quiser
+
+    /*
+    Comentário maior com várias linhas
+    */
+
+    /***
+     * Esse é o método main
+     * @param args parametro usado para o metodo main
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Dados Impressos");
+    }
+}
