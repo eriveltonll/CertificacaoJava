2f80041b42f92b91baa706fb10b76d00ed036012
Erivelton Laia <eriveltonll@gmail.com>
2022-11-30 19:15:30 -0300

    operadores 2

--- JavaOnline/src/Operadores.java
+++ JavaOnline/src/Operadores.java
@@ -8,5 +8,21 @@ public class Operadores {
         System.out.println("A subtração é: " + (n1-n2));
         System.out.println("A divisão é: " + (n1/n2));
         System.out.println("A multiplicação é: " + (n1*n2));
+
+
+        double d = 10d;
+        float f = 20f;
+        System.out.println("Antes de converter " +f);
+        f = (float) d;
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println("Depois de converter " +f);
+
+
+        System.out.println("Resto de Divisão Par "+(10%2));
+        System.out.println("Resto de Divisão Impar "+(11%2));
+
+        System.out.println();
+
     }
 }
